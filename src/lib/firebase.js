9135c5f4fff8f43715667a10406c5c0fb7156412
Sunil3628 <sunilkumarsahu726189@gmail.com>
2026-05-21 16:10:30 +0530

    hide firebase keys

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZi3UqDwlu4bt9vkTgPRebWGQlNy9EeHc",
-  authDomain: "todo-project-8346e.firebaseapp.com",
-  projectId: "todo-project-8346e",
-  storageBucket: "todo-project-8346e.firebasestorage.app",
-  messagingSenderId: "383831091894",
-  appId: "1:383831091894:web:f6fa1d3b9136112e891417",
+  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PUBLIC_FIREBASE_APP_ID,
+
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
